8fe57e70853640180d8815fb43b8b2fce0e66485
juanroy <juanroycouto@gmail.com>
2017-02-11 16:49:12 +0100

    Update cronhilocolademonio.py

--- cronhilocolademonio.py
+++ cronhilocolademonio.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     print '\n', threading.currentThread().getName(), 'Lanzado thread principal...'
 
     #Relleno la cola con la informacion de los cron a ejecutar
-    conn = pymssql.connect(server='172.16.5.1', user='undanet', password='undanet2015', port=1433, database='neuman')
+    conn = pymssql.connect(server='', user='', password='', port=, database='')
     query = 'select cast(sp.id as varchar(3)), sp.alias, sp.cronCommand, sp.description, cast(sp.platform_id as varchar(3)) from neuman.dbo.stock_platform sp join neuman.dbo.platform p on p.id = sp.platform_id where sp.active = 1 and sp.is_webservice = 0 and p.active = 1'
     #while True:
     for i in range(4):
